4633f5cacd7963d23a7bdd5bb4f6d37f0edce576
Tobi <tfortobi@users.noreply.github.com>
2020-04-18 03:55:52 -0400

    update

--- netflixscript.js
+++ netflixscript.js
@@ -1,8 +1,16 @@
-let acc = document.getElementsByClassName("accordion")
+
+//WORK ON THE FREQUENTLY ASKED QUESTIONS SECTION
+
+let acc = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
-
+        let pan = this.nextElementSibling;
+        if (pan.style.display === "block") {
+            pan.style.display = "none";
+        } else {
+            pan.style.display = "block";
+        }
 
     })
 
